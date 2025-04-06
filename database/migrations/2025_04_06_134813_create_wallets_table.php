db4a4a59387f0cfd886755595e011000dd99fbce
Andrew Ferrer <andrewferrer80@gmail.com>
2025-04-07 00:18:02 +0800

    initial migration

---
+++ database/migrations/2025_04_06_134813_create_wallets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->enum('type', ['cash', 'bank', 'digital', 'credit_card']);
+            $table->decimal('balance', 15, 2)->default(0);
+            $table->decimal('credit_limit', 15, 2)->nullable();
+            $table->boolean('include_in_total')->default(true);
+            $table->timestamps();
+
+            $table->index(['id', 'name', 'type', 'balance', 'credit_limit']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
---
+++ database/migrations/2025_04_06_144703_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('wallet_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->enum('type', ['income', 'expense', 'transfer']);
+            $table->decimal('amount', 15, 2);
+            $table->string('note')->nullable();
+            $table->longText('image_url')->nullable();
+            $table->date('date');
+            $table->timestamps();
+
+            $table->index(['id', 'wallet_id', 'category_id', 'type', 'amount', 'date']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
